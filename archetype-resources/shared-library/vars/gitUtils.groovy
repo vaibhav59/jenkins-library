4cd8868a14aa1f24277d3c9e407c0d2896809f8d
vaibhav59 <vaibhavjha_1987@yahoo.co.in>
2018-07-26 14:02:54 +0530

    added groovy code

---
+++ archetype-resources/shared-library/vars/gitUtils.groovy
@@ -0,0 +1,16 @@
+// vars/gitUtils.groovy
+
+String getCommitId(boolean shortId = false, String args = 'HEAD') {
+    String commitId = sh(returnStdout: true, script: "git rev-parse ${args}").trim()
+    return shortId ? commitId[0..6] : commitId
+}
+
+
+
+/**
+ * Retrieve current branch name.
+ * @return branch name.
+ */
+String getBranchName() {
+    return env.BRANCH_NAME ?: sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+}
