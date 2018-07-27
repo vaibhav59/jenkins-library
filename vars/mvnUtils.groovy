f6ba01af589b51c76220730a2c7ce0539131e032
root <root@D1ML23449.mindtree.com>
2018-07-27 13:47:00 +0530

    version file added

--- vars/mvnUtils.groovy
+++ vars/mvnUtils.groovy
@@ -1,22 +1,6 @@
 String getPomProperty(final Map args) {
-    utils.validateArgs args, ['property']
-
     final String pathToPom = args.pathToPom ?: 'pom.xml'
     final matcher = readFile(pathToPom) =~ "<${args.property}>(.+)</${args.property}>"
     String version = matcher ? matcher[0][1] : null
     return version
-}
-s
-static void validateArgs(final Map args, final List<String> keys) throws InvalidArgumentsException {
-    final List nullProps = []
-
-    for (key in keys) {
-        if (args[key] == null) {
-            nullProps.add key
-        }
-    }
-
-    if (nullProps.size()) {
-        throw new InvalidArgumentsException("${nullProps.join(', ')} must be defined!")
-    }
 }
