4d24a1893e960a6f668e6cc594c27cf3f88cf2a2
root <root@D1ML23449.mindtree.com>
2018-07-27 18:17:17 +0530

    modified slack

--- vars/slackUtils.groovy
+++ vars/slackUtils.groovy
@@ -1,63 +1,7 @@
 void notify(final Map args) {
-    utils.validateArgs args, ['message', 'credentials', 'channel', 'team']
+if (!dryRun){
 
-    final boolean addBuildInfo = args.addBuildInfo || args.addBuildInfo == null || args.addBuildInfo == ''
-    final String date = sh(returnStdout: true, script: 'date').trim()
-    final String message = addBuildInfo ?
-            "[${date} :: ${env.JOB_NAME} :: <${env.BUILD_URL}|${env.BUILD_NUMBER}>] ${args.message} (<${env.BUILD_URL}|Open>)" :
-            args.message
-    String color = 'good'
+  slackSend (baseUrl: "https://${args.team}/services/hooks/jenkins-ci/" ,color: args.level , message: "Success: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})", channel: args.channel, teamDomain: args.team, tokenCredentialId: args.credentials)
+  }
 
-    withCredentials([string(credentialsId: args.credentials, variable: 'token')]) {
-        switch (args.level) {
-            case 'error':
-            case 'danger':
-                color = 'danger'
-                break
-            case 'warn':
-            case 'warning':
-                color = 'warning'
-                break
-            case 'log':
-            case 'info':
-            case 'good':
-                color = 'good'
-                break
-        }
-
-        def dryRun = (args.dryRun) ?: (env.slackIsInDryRun?.toBoolean()) ?: false
-
-        if (!dryRun) {
-            try {
-                httpRequest(
-                        httpMode: 'POST',
-                        url: "https://${args.team}.slack.com/services/hooks/jenkins-ci?token=${env.token}",
-                        responseHandle: 'NONE',
-                        requestBody: """{
-                            "channel": "${args.channel}",
-                            "attachments": [
-                                {
-                                    "text": "${message}",
-                                    "fallback": "${message}",
-                                    "color": "${color}",
-                                    "mrkdwn_in": [
-                                        "pretext",
-                                        "text",
-                                        "fields"
-                                    ]
-                                }
-                            ],
-                            "link_names": "1"
-                        }"""
-                )
-            } catch (IllegalStateException ex) {
-                echo "Failed to publish message on Slack channel ${args.channel}. Caused by: ${ex.message}"
-            }
-        }
-
-        echo "[${date} :: ${color}] ${args.message}"
-    }
 }
-
-return this
-
