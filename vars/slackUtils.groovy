7fc492d4af4f1f42997897ad91fd4199e6dc652f
root <root@D1ML23449.mindtree.com>
2018-07-27 18:20:28 +0530

    modified slack

--- vars/slackUtils.groovy
+++ vars/slackUtils.groovy
@@ -1,5 +1,5 @@
 void notify(final Map args) {
-if (!dryRun){
+if (!args.dryRun){
 
   slackSend (baseUrl: "https://${args.team}/services/hooks/jenkins-ci/" ,color: args.level , message: "Success: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})", channel: args.channel, teamDomain: args.team, tokenCredentialId: args.credentials)
   }
