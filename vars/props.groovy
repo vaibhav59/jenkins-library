ff98bf4a7ed069ed14d95d5acc9e7bc32d0718b7
root <root@localhost.localdomain>
2018-07-29 07:03:47 -0400

    Done

--- vars/props.groovy
+++ vars/props.groovy
@@ -1,4 +1,4 @@
 // vars/props.groovy
-def logrotate( Map args, final String option) {
+def logrotate( Map args, option) {
     properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: "${args.day}", numToKeepStr: "${args.build}")), disableConcurrentBuilds(), [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], option]) 
 }
